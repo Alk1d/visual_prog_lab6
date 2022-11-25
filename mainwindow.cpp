d40b520cd3b532f3539ce0698c10bc66c096e436
Alk1d <108924851+Alk1d@users.noreply.github.com>
2022-11-25 10:18:17 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,68 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QPushButton"
+#include "QHBoxLayout"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QWidget *widget = new QWidget;
+
+    QPushButton *button1 = new QPushButton("Show Error");
+    QPushButton *button2 = new QPushButton("Show Warning");
+    QPushButton *button3 = new QPushButton("Show Information");
+
+    QHBoxLayout *Layout = new QHBoxLayout;
+    Layout->addWidget(button1);
+    Layout->addWidget(button2);
+    Layout->addWidget(button3);
+
+    widget->setLayout(Layout);
+
+     connect( button1, &QPushButton::clicked, this, &MainWindow::onErrorButtonClicked );
+     connect( button2, &QPushButton::clicked, this, &MainWindow::onWarningButtonClicked );
+     connect( button3, &QPushButton::clicked, this, &MainWindow::onInformationButtonClicked );
+
+
+    widget->show();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onErrorButtonClicked()
+{
+    int ret = QMessageBox::critical(this, tr("My Application"),
+                                   tr("The document has been modified.\n"
+                                      "Do you want to save your changes?"),
+                                   QMessageBox::Save | QMessageBox::Discard
+                                   | QMessageBox::Cancel,
+                                   QMessageBox::Save);
+}
+
+void MainWindow::onWarningButtonClicked()
+{
+    int ret = QMessageBox::warning(this, tr("My Application"),
+                                   tr("The document has been modified.\n"
+                                      "Do you want to save your changes?"),
+                                   QMessageBox::Save | QMessageBox::Discard
+                                   | QMessageBox::Cancel,
+                                   QMessageBox::Save);
+}
+
+void MainWindow::onInformationButtonClicked()
+{
+    int ret = QMessageBox::information(this, tr("My Application"),
+                                   tr("The document has been modified.\n"
+                                      "Do you want to save your changes?"),
+                                   QMessageBox::Save | QMessageBox::Discard
+                                   | QMessageBox::Cancel,
+                                   QMessageBox::Save);
+}
+
